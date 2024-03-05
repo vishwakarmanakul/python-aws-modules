0b729ff4822e4497278fef1771a20fe79b1fc3b0
nakul <nakul.vishwakarma78@gmail.com>
2024-03-05 16:34:18 +0530

    updated hello.py

--- NonPremetiveDataTypes/hello.py
+++ NonPremetiveDataTypes/hello.py
@@ -1,3 +1,6 @@
-test_dict = {"Arushi": 22, "Mani": 21, "Haritha": 21}
-new={i:j for i,j in test_dict.items() if i!="Mani"}
-print(new)
+list=[5,2,3,5,7,3,21,2,4,6,79]
+l= list.copy()
+reversed_=[]
+for i in range(len(list)):
+    a=list.pop()
+    reversed_.append(a)
